89bfaa4f2d529b4a0e73e0bfecc9b5343413a438
Erik Aalbers <erik@erikaalbers.com>
2017-03-04 20:45:19 +0000

    Erik's janky code

--- app/taunt.py
+++ app/taunt.py
@@ -9,12 +9,12 @@ import random
 def taunt(feels):
     
     if taunt(feels) == "MAD":
-        taunt = ['GRRR', 'I\'M MAD BRO', '#TABLEFLIP', '#RAGEQUIT']
+        tauntOptions = ['GRRR', 'I\'M MAD BRO', '#TABLEFLIP', '#RAGEQUIT']
     elif taunt(feels) == "SAD":
-        taunt = ['SAD', 'WHY YOU DO THIS', ':\'(']
+        tauntOptions = ['SAD', 'WHY YOU DO THIS', ':\'(']
     elif taunt(feels) == "HUNGRY":
-        taunt = ['FOOD PLEASE', 'I\'M HANGRY']
+        tauntOptions = ['FOOD PLEASE', 'I\'M HANGRY']
     else:
-        taunt = ['I\'MMMA WIN, JK', 'LOLOL', 'Come Get Me!']
+        tauntOptions = ['I\'MMMA WIN, JK', 'LOLOL', 'Come Get Me!']
     
-    return random.choice(taunt)
+    return random.choice(tauntOptions)
