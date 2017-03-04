6050cdd691240e391e7e9bd734adb5580dea2f66
flytewizard <domo.charlebois@gmail.com>
2017-03-04 14:58:45 -0800

    DC: Added Food Logic

--- app/plzEatFood.py
+++ app/plzEatFood.py
@@ -1,21 +1,21 @@
 # Collect Food Code
 
 # Find snake ID
-# def myHeadCheck(data):
-#
-#    for snake in data['snakes']:
-#
-#        #this is your snake
-#        if snake['id'] == snake['you']:
-#
-#            #return the location of our head
-#            return snake['coords'][0, 1]
-#
+def myHeadCheck(data):
+
+    for snake in data['snakes']:
+
+        #this is your snake
+        if snake['id'] == snake['you']:
+
+            #return the location of our head
+            return snake['coords'][0, 1]
+
 # Collect food data
-# def foodCheck(data):
-#
-#    for food in data['food']:
-#
-#        return food['coords'][0, 1]
-#
+def foodCheck(data):
+
+    for food in data['food']:
+
+        return food['coords'][0, 1]
+
 # Determine closes food piece
