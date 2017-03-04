bb480a2d42795f16ecc7e27e13b776d354bdb8a1
Erik Aalbers <erik@erikaalbers.com>
2017-03-04 20:11:15 +0000

    Erik's janky code

---
+++ app/taunt.py
@@ -0,0 +1,8 @@
+# Different Game Taunts
+taunt = ['GRRR', 'I\'MMMA WIN, JK', 'LOLOL', 'Come Get Me!']
+
+
+#if data['snakes']['health_points'] == 98:
+#        taunt = "I'm HUNGRY" 
+#    else:
+#        taunt = random.choice(taunt)
