d9a2299a913620880079b23b8d0e2ec0a67db743
flytewizard <domo.charlebois@gmail.com>
2017-03-04 12:13:37 -0800

    DC: Fixed Erik's Code

--- app/taunt.py
+++ app/taunt.py
@@ -3,6 +3,6 @@ taunt = ['GRRR', 'I\'MMMA WIN, JK', 'LOLOL', 'Come Get Me!']
 
 
 #if data['snakes']['health_points'] == 98:
-#        taunt = "I'm HUNGRY" 
-#    else:
-#        taunt = random.choice(taunt)
+#   taunt = "I'm HUNGRY"
+#else:
+#   taunt = random.choice(taunt)
