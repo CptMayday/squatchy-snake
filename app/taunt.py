bd219b6c41540b4b6067633cf966395ea265af84
Erik Aalbers <erik@erikaalbers.com>
2017-03-04 20:35:05 +0000

    EA: Add feels to taunt

--- app/taunt.py
+++ app/taunt.py
@@ -1,7 +1,20 @@
 import random
 
-# Different Game Taunts
-def taunt():
-    taunt = ['GRRR', 'I\'MMMA WIN, JK', 'LOLOL', 'Come Get Me!']
+# Different Game Taunts, add a feeling to get a random taunt matching this feeling
+# Accepted feels
+# - MAD
+# - SAD
+# - HUNGRY
+
+def taunt(feels):
+    
+    if taunt(feels) == "MAD":
+        taunt = ['GRRR', 'I\'M MAD BRO', '#TABLEFLIP', '#RAGEQUIT']
+    elif taunt(feels) == "SAD":
+        taunt = ['SAD', 'WHY YOU DO THIS', ':\'(']
+    elif taunt(feels) == "HUNGRY":
+        taunt = ['FOOD PLEASE', 'I\'M HUNGRY']
+    else:
+        taunt = ['I\'MMMA WIN, JK', 'LOLOL', 'Come Get Me!']
     
     return random.choice(taunt)
