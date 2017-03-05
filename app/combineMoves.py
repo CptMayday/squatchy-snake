6eed9f0b5a192d55e9ead11f98701ff95938c120
Hayward Peirce <hayward@referralsaasquatch.com>
2017-03-05 02:02:04 +0000

    HP: fix

--- app/combineMoves.py
+++ app/combineMoves.py
@@ -5,4 +5,5 @@ def move(dontDieMoves, foodMove):
     for dontDieMove in dontDieMoves:
         if dontDieMove == foodMove:
             return foodMove
-        else return random.choice(dontDieMoves)
+        else:
+            return random.choice(dontDieMoves)
